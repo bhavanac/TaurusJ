7dd14adcebb7ea9ec110e1a24b4e4c3d9b276d63
Bhavana Challa <bchalla@linkedin.com>
2015-06-27 11:54:44 -0700

    Add Main file

---
+++ src/edu/ds/practice/Main.java
@@ -0,0 +1,9 @@
+package edu.ds.practice;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("My first GitHub commit !");
+    }
+}
